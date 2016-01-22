2f7eb3bed175a9f0a4caac961bf023a06fe70dc6
Ivan Shen <ivanshen@inside-173-157.wireless.utoronto.ca>
2016-01-22 18:40:57 -0500

    Updated website

---
+++ js/app.js
@@ -0,0 +1,43 @@
+var app = angular.module('MainApp', ['ngRoute']);
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/home.html'
+    })
+        .when('/projects', {
+            templateUrl: 'views/projects.html',
+            controller: 'ProjectController'
+    })
+        .when('/blog', {
+            templateUrl: 'views/blog.html'
+    })
+        .otherwise({
+            redirectTo: '/'
+    });
+});
+
+app.controller('ProjectController', function ($scope){
+    $scope.project = {
+        BabyBuddy: {
+            "title": "BabyBuddy",
+            "description": 'An educational resource for pregnant women.',
+            "link": 'ivanshen.me/babybuddy'
+        },
+        SwapWithMe: {
+            "title": 'SwapWithMe',
+            "description": 'A platform for students to sell or swap their textbooks with other students.',
+            link: 'swapwithme.meteor.com'
+        },
+        HelpMeUp: {
+            "title": 'Help Me Up',
+            "description": "A patient monitoring Android app that uses a neural net to alert caregivers of a patient's fall and circumstances.",
+            "link": "http://devpost.com/software/helpmeup"
+        },
+        WhoAreYouReally: {
+            "title": "Who Are You Really?",
+            "description":"A learning styles quiz that breaks down a user’s best learning styles in percentages.",
+            "link": "https://github.com/ivanshen/Who-Are-You-Really"
+        }
+    };
+});
+    
--- js/fade-out_scrolling.js
+++
@@ -1,9 +0,0 @@
-jQuery(function($) {
-    var divs = $('.About_me');
-    $(window).on('scroll', function() {
-        var st = $(this).scrollTop();
-        divs.css({ 
-            'opacity' : st/150
-        }); 
-    });
-});
